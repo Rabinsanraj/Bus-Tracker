b037133770da35e0abdf31f104007107f3aa7870
Rabinsanraj <robinsanraj207@gmail.com>
2025-04-26 11:51:18 +0530

    'mod'

--- src/Pages/Options.jsx
+++ src/Pages/Options.jsx
@@ -2,7 +2,12 @@ export function Options(){
     return(
         <>
         <div className="container text-center">
-            <p className="text-white">This is a options page</p>
+            <div className="row">
+                <div className="col">Report</div>
+                <div className="col">Make a Call</div>
+                <div className="col">Mail</div>
+                <div className="col">Suggestion Box</div>
+            </div>
         </div>
         </>
     )
